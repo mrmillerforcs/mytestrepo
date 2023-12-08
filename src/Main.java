0731721b98b2086b1fcc976932f7accdcc220277
teacher <bths@bths.edu>
2023-12-08 15:51:10 -0500

    second partner adding code

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("our first shared repo");
+        System.out.println("this is the other partner adding some code");
     }
 }
