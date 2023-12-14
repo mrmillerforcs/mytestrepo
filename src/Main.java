5b3d6f854c6fecd8f1c2c89f1fff4bdff4bbe96c
laptop <sample@bths.edu>
2023-12-14 09:07:57 -0500

    added print statement

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,6 @@ public class Main {
         System.out.println("our first shared repo");
         System.out.println("this is the other partner adding some code");
         System.out.println("what's up");
+        System.out.println("not much!");
     }
 }
