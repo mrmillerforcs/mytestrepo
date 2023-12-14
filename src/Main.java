c61053522e40740eefb01986fda2d6fb8a11286c
teacher <bths@bths.edu>
2023-12-14 12:00:39 -0500

    updated string

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("our first shared repo");
         System.out.println("partner 1 just changed this string");
-        System.out.println("what's up");
+        System.out.println("LOOK AT ME");
         System.out.println("partner 2 is changing this line");
     }
 }
